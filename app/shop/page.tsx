e96bf08f562188f6c886b60bec923b9310cda931
Andre <andre@Andres-MacBook-Pro.local>
2026-06-13 17:22:21 +0200

    update pages, add shop, remove cook

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -4,12 +4,12 @@ import ToolbarWin from "@/components/toolbarwin";
 import Space from "@/components/space";
 import { useProtectedRoute } from "@/hooks/use-protected-route";
 
-export default function CookPage() {
+export default function ShopPage() {
     useProtectedRoute();
     return (
         <>
             <Space size={40} />
-            <ToolbarWin title="Cook" />
+            <ToolbarWin title="Shopping list" />
         </>
     )
 }
